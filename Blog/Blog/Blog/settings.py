d816e04becdf61a7d7b6949d1ae79fada1633e64
mobeen777 <mobeen.mobeen777@gmail.com>
2022-10-27 10:09:43 +0500

    'minor changes'

--- Blog/Blog/Blog/settings.py
+++ Blog/Blog/Blog/settings.py
@@ -133,5 +133,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'Blog/media')
 MEDIA_URL = '/media/'
--- Blog/Blog/story/validators.py
+++ Blog/Blog/story/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_file_size(value):
     filesize = value.size
 
-    if filesize > 10485760:
-        raise ValidationError("You cannot upload file more than 10Mb")
+    if filesize > 5242880:
+        raise ValidationError("You cannot upload file more than 5Mb")
     else:
         return value
